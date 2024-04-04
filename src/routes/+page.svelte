5d93c617bcc693d5211150c67cb5c4765a186ee6
Vojtech Simetka <vojtech@simetka.cz>
2024-04-04 18:30:03 +0200

    refactor: use pcss, update dependencies

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
 <h1>A design system for purists</h1>
-<p>Radically minimal UI components, that you can copy directly into your project. Diète is for uncompromising, free-spirited app makers who want to remain independent.</p>
+<p>
+	Radically minimal UI components, that you can copy directly into your project. Diète is for
+	uncompromising, free-spirited app makers who want to remain independent.
+</p>
