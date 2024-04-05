8caa419d836fce7e29e8f9b31667fe4b9b26c8ee
Attila Gazso <230163+agazso@users.noreply.github.com>
2024-04-05 16:23:58 +0200

    wip: custom preprocess step

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,29 @@
 import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
+function cssPreprocess() {
+	const vite = vitePreprocess()
+	const viteScript = vite.script
+	const viteStyle = vite.style
+	return {
+		async script(args) {
+			return viteScript(args)
+		},
+		async style(args) {
+			const preprocessedStyle = await viteStyle(args)
+			if (args.filename.includes('/routes/') && args.filename.endsWith('.svelte')) {
+				console.debug('style', { filename: args.filename, css: preprocessedStyle.code })
+			}
+			return preprocessedStyle
+		}
+	}
+}
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [cssPreprocess()],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vitest/config'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 	},
+	css: {
+		postcss: {
+			plugins: [autoprefixer],
+		}
+	}
 })
