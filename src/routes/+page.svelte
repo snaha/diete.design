068eb838586d46b0728aa82c74cf6254ab0dc0aa
Attila Gazso <230163+agazso@users.noreply.github.com>
2024-04-08 14:41:07 +0200

    wip: remove unused code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,3 @@
 	Radically minimal UI components, that you can copy directly into your project. Diète is for
 	uncompromising, free-spirited app makers who want to remain independent.
 </p>
-
-<style>
-.h1 {
-	font-size: 16px;
-	transform: scale(0.5);
-}
-</style>
