c49a4db8a01dc54f3105c386e00455ff84b6efea
hlavkat <137988631+hlavkat@users.noreply.github.com>
2024-04-09 14:04:47 +0200

    feat: checkbox component

--- src/lib/components/checkbox.svelte
+++ src/lib/components/checkbox.svelte
@@ -8,15 +8,9 @@
 </script>
 
 <div class="root">
-	<input type="checkbox" {...restProps} id={labelFor} class="checkmark"/>
+	<input type="checkbox" {...restProps} id={labelFor} class="checkmark" />
 	<svg width="24" height="24" viewBox="0 0 24 24">
-		<rect
-			width="24"
-			height="24"
-			fill="black"
-			fill-opacity="0.7"
-			style="mix-blend-mode:multiply"
-		/>
+		<rect width="24" height="24" fill="black" fill-opacity="0.7" style="mix-blend-mode:multiply" />
 		<path
 			d="M9.75 18L3 11.25L4.0605 10.1895L9.75 15.8782L19.9395 5.68945L21 6.74995L9.75 18Z"
 			fill="#F9F9F9"
@@ -32,20 +26,22 @@
 		align-items: center;
 		gap: 0.5rem;
 		position: relative;
-	}	
+	}
 	input[type='checkbox'] {
 		appearance: none;
 		margin: 0;
-		&:checked{
+		&:checked {
 			appearance: none;
 		}
-		&:checked.checkmark{
-            border: none;
-        }
-		&:checked + svg{
+		&:checked.checkmark {
+			border: none;
+		}
+		&:checked + svg {
 			display: block;
 		}
-		&:disabled.checkmark,&:disabled ~ svg,&:disabled ~ .label{
+		&:disabled.checkmark,
+		&:disabled ~ svg,
+		&:disabled ~ .label {
 			opacity: 0.25;
 			cursor: not-allowed;
 		}
@@ -54,21 +50,21 @@
 		display: flex;
 		align-items: flex-start;
 		width: 1.5rem;
-		height: 1.5rem;		
+		height: 1.5rem;
 		border: 1px solid var(--colors-ultra-high);
 		border-radius: 0.25rem;
 		cursor: pointer;
 		z-index: 1;
 	}
-	svg{
+	svg {
 		position: absolute;
-		top: 0,75rem;
+		top: 0, 75rem;
 		left: 0.75rem;
 		border-radius: 0.25rem;
 		display: none;
 	}
 
-	.label{
+	.label {
 		cursor: pointer;
 	}
 </style>
