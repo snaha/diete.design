6c0bd5119a7156cd6379a7783d11501893e46682
Attila Gazso <230163+agazso@users.noreply.github.com>
2024-04-09 14:04:52 +0200

    feat: custom css (#13)
    * wip: custom preprocess step
    
    * wip: remove script from preprocessor
    
    * wip: write file
    
    * wip: remove unused code
    
    * fix: linting
    
    * fix: nested paths
    
    * fix: linting
    
    * fix: postcss config

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,38 @@
 import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import fs from 'fs'
+
+const DIST_DIR = './css'
+
+function cssPreprocess() {
+	const vite = vitePreprocess()
+	const viteStyle = vite.style
+
+	return {
+		...vite,
+		async style(args) {
+			const preprocessedStyle = await viteStyle(args)
+			const match = args.filename.match(/^.*\/src\/lib\/components(.*\/)(.*)\.svelte/)
+			if (match && match.length === 3) {
+				const compPath = match[1]
+				const compName = match[2]
+
+				const path = `${DIST_DIR}${compPath}`
+				fs.mkdirSync(path, { recursive: true })
+
+				const cssFilePath = `${path}/${compName}.css`
+				fs.writeFileSync(cssFilePath, preprocessedStyle.code)
+			}
+			return preprocessedStyle
+		},
+	}
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [cssPreprocess()],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
