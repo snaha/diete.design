62ef391e4890ea281edad59da32d98cbbffd36ff
Attila Gazso <230163+agazso@users.noreply.github.com>
2024-04-12 18:35:49 +0200

    wip: import css files as asset (#21)
    * wip: import css files as asset
    
    * fix: add declare to global d.ts
    
    * wip: add d.ts for css modules
    
    * fix: raw import path changed inside static
    
    * fix: add separate css test page
    
    * fix: moved css test to csstest folder
    
    * fix: use dynamic loading with fetch
    
    * fix: add css as exception to linters
    
    * fix: remove .d.ts
    
    * fix: remove .d.ts
    
    * fix: use /static/generated/css

---
+++ src/routes/csstest/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+	let css: string | undefined
+
+	onMount(async () => {
+		const response = await fetch('/generated/css/ui/button.css')
+		css = await response.text()
+	})
+</script>
+
+<code>
+	{css ? css : 'Loading...'}
+</code>
