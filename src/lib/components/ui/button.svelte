7e233147cb24331799e00c063c501778d61da8f7
Vojtech Simetka <vojtech@simetka.cz>
2024-04-12 10:52:01 +0200

    feat: button component (#9)
    * feat: button component
    
    * added dark-overlay variant and sizes
    
    added dark-overlay variant and sizes
    
    * chore: remove duplicate ghost button
    
    * fix: undefined class
    
    * fix: extract button font-family to variable and use sans-serif instead of Arial
    
    ---------
    
    Co-authored-by: hlavkat <137988631+hlavkat@users.noreply.github.com>

---
+++ src/lib/components/ui/button.svelte
@@ -0,0 +1,149 @@
+<script lang="ts">
+	import type { HTMLAnchorAttributes, HTMLButtonAttributes } from 'svelte/elements'
+	type Variant = 'strong' | 'secondary' | 'ghost' | 'overlay' | 'darkoverlay'
+	type Size = 'default' | 'large' | 'compact' | 'small'
+	type Props = {
+		variant?: Variant
+		active?: boolean
+		class?: string | null
+		size?: Size
+	}
+	interface AnchorElement extends HTMLAnchorAttributes, Props {
+		href?: HTMLAnchorAttributes['href']
+		type?: never
+		disabled?: never
+	}
+
+	interface ButtonElement extends HTMLButtonAttributes, Props {
+		type?: HTMLButtonAttributes['type']
+		href?: never
+		disabled?: boolean
+	}
+	let {
+		size = 'default',
+		variant = 'strong',
+		active,
+		disabled,
+		href,
+		class: className = '',
+		...restProps
+	}: AnchorElement | ButtonElement = $props()
+</script>
+
+<svelte:element
+	this={href ? 'a' : 'button'}
+	class={`${size} ${variant} ${className}`}
+	{href}
+	class:active
+	{disabled}
+	{...restProps}
+>
+	<slot />
+</svelte:element>
+
+<style lang="postcss">
+	button,
+	a {
+		display: inline-flex;
+		justify-content: center;
+		align-items: center;
+		gap: 0.5rem;
+		border-radius: 0.25rem;
+		border: 1px solid var(--colors-ultra-high);
+		cursor: pointer;
+		text-decoration: none;
+		font-family: var(--font-family-sans-serif);
+		font-style: normal;
+		font-weight: 400;
+		&:disabled {
+			cursor: not-allowed;
+			opacity: 0.25;
+		}
+	}
+	.default {
+		min-width: 3rem;
+		padding: 0.75rem;
+		font-size: 1rem;
+		line-height: 1.5rem;
+		letter-spacing: 0.02rem;
+	}
+	.large {
+		min-width: 3.5rem;
+		padding: 0.75rem;
+		font-size: 1.5rem;
+		line-height: 2rem;
+		letter-spacing: 0.03rem;
+	}
+	.compact {
+		min-width: 2.5rem;
+		padding: 0.5rem;
+		font-size: 1rem;
+		line-height: 1.5rem;
+		letter-spacing: 0.02rem;
+	}
+	.small {
+		gap: 0.25rem;
+		min-width: 2rem;
+		padding: 0.5rem;
+		font-size: 0.75rem;
+		line-height: 1rem;
+		letter-spacing: 0.0375rem;
+	}
+
+	.strong {
+		background: var(--colors-ultra-high);
+		color: var(--colors-ultra-low);
+
+		&:active:not(:disabled),
+		&.active:not(:disabled) {
+			border: 1px solid var(--colors-high);
+			background: var(--colors-high);
+			color: var(--colors-base);
+		}
+	}
+	.secondary {
+		border: 1px solid var(--colors-ultra-high);
+		background: none;
+		color: var(--colors-ultra-high);
+
+		&:active:not(:disabled),
+		&.active:not(:disabled) {
+			border: 1px solid var(--colors-high);
+			color: var(--colors-high);
+		}
+	}
+	.ghost {
+		border: 1px solid transparent;
+		background: transparent;
+		color: var(--colors-ultra-high);
+
+		&:active:not(:disabled),
+		&.active:not(:disabled) {
+			border: 1px solid var(--colors-low);
+			background: var(--colors-low);
+			color: var(--colors-high);
+		}
+	}
+	.overlay {
+		border: 1px solid var(--colors-base);
+		background: var(--colors-base);
+		color: var(--colors-ultra-high);
+
+		&:active:not(:disabled),
+		&.active:not(:disabled) {
+			border: 1px solid var(--colors-low);
+			background: var(--colors-low);
+			color: var(--colors-high);
+		}
+	}
+	.darkoverlay {
+		border: 1px solid var(--colors-dark-overlay);
+		background: var(--colors-dark-overlay);
+		color: var(--colors-base);
+		&:active:not(:disabled),
+		&.active:not(:disabled) {
+			border: 1px solid var(--colors-top);
+			background: var(--colors-top);
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,205 @@
+<script lang="ts">
+	import { Checkmark, ArrowRight } from 'carbon-icons-svelte'
+
+	import Button from '$lib/components/ui/button.svelte'
+</script>
+
 <h1>A design system for purists</h1>
 <p>
 	Radically minimal UI components, that you can copy directly into your project. Diète is for
 	uncompromising, free-spirited app makers who want to remain independent.
 </p>
+
+<section>
+	<h1>Button</h1>
+	<div class="row">
+		<div class="col">
+			<Button>Strong</Button>
+			<Button active>Strong active</Button>
+			<Button disabled>Strong disabled</Button>
+		</div>
+		<div class="col">
+			<Button><Checkmark size={24} /></Button>
+			<Button active><Checkmark size={24} /></Button>
+			<Button disabled><Checkmark size={24} /></Button>
+		</div>
+		<div class="col">
+			<Button><Checkmark size={24} />Strong</Button>
+			<Button active><Checkmark size={24} />Strong active</Button>
+			<Button disabled><Checkmark size={24} />Strong disabled</Button>
+		</div>
+		<div class="col">
+			<Button>Strong<ArrowRight size={24} /></Button>
+			<Button active>Strong active<ArrowRight size={24} /></Button>
+			<Button disabled>Strong disabled<ArrowRight size={24} /></Button>
+		</div>
+		<div class="col">
+			<Button variant="secondary">Secondary</Button>
+			<Button variant="secondary" active>Secondary active</Button>
+			<Button variant="secondary" disabled>Secondary disabled</Button>
+		</div>
+		<div class="col">
+			<Button variant="ghost">Ghost</Button>
+			<Button variant="ghost" active>Ghost active</Button>
+			<Button variant="ghost" disabled>Ghost disabled</Button>
+		</div>
+		<div class="col">
+			<Button variant="overlay">Overlay</Button>
+			<Button variant="overlay" active>Overlay active</Button>
+			<Button variant="overlay" disabled>Overlay disabled</Button>
+		</div>
+		<div class="col">
+			<Button variant="darkoverlay">Dark overlay</Button>
+			<Button variant="darkoverlay" active>Dark overlay active</Button>
+			<Button variant="darkoverlay" disabled>Dark overlay disabled</Button>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col">
+			<Button size="large">Strong</Button>
+			<Button size="large" active>Strong active</Button>
+			<Button size="large" disabled>Strong disabled</Button>
+		</div>
+		<div class="col">
+			<Button size="large"><Checkmark size={32} /></Button>
+			<Button size="large" active><Checkmark size={32} /></Button>
+			<Button size="large" disabled><Checkmark size={32} /></Button>
+		</div>
+		<div class="col">
+			<Button size="large"><Checkmark size={32} />Strong</Button>
+			<Button size="large" active><Checkmark size={32} />Strong active</Button>
+			<Button size="large" disabled><Checkmark size={32} />Strong disabled</Button>
+		</div>
+		<div class="col">
+			<Button size="large">Strong<ArrowRight size={32} /></Button>
+			<Button size="large" active>Strong active<ArrowRight size={32} /></Button>
+			<Button size="large" disabled>Strong disabled<ArrowRight size={32} /></Button>
+		</div>
+		<div class="col">
+			<Button size="large" variant="secondary">Secondary</Button>
+			<Button size="large" variant="secondary" active>Secondary active</Button>
+			<Button size="large" variant="secondary" disabled>Secondary disabled</Button>
+		</div>
+		<div class="col">
+			<Button size="large" variant="ghost">Ghost</Button>
+			<Button size="large" variant="ghost" active>Ghost active</Button>
+			<Button size="large" variant="ghost" disabled>Ghost disabled</Button>
+		</div>
+		<div class="col">
+			<Button size="large" variant="overlay">Overlay</Button>
+			<Button size="large" variant="overlay" active>Overlay active</Button>
+			<Button size="large" variant="overlay" disabled>Overlay disabled</Button>
+		</div>
+		<div class="col">
+			<Button size="large" variant="darkoverlay">Dark overlay</Button>
+			<Button size="large" variant="darkoverlay" active>Dark overlay active</Button>
+			<Button size="large" variant="darkoverlay" disabled>Dark overlay disabled</Button>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col">
+			<Button size="compact">Strong</Button>
+			<Button size="compact" active>Strong active</Button>
+			<Button size="compact" disabled>Strong disabled</Button>
+		</div>
+		<div class="col">
+			<Button size="compact"><Checkmark size={24} /></Button>
+			<Button size="compact" active><Checkmark size={24} /></Button>
+			<Button size="compact" disabled><Checkmark size={24} /></Button>
+		</div>
+		<div class="col">
+			<Button size="compact"><Checkmark size={24} />Strong</Button>
+			<Button size="compact" active><Checkmark size={24} />Strong active</Button>
+			<Button size="compact" disabled><Checkmark size={24} />Strong disabled</Button>
+		</div>
+		<div class="col">
+			<Button size="compact">Strong<ArrowRight size={24} /></Button>
+			<Button size="compact" active>Strong active<ArrowRight size={24} /></Button>
+			<Button size="compact" disabled>Strong disabled<ArrowRight size={24} /></Button>
+		</div>
+		<div class="col">
+			<Button size="compact" variant="secondary">Secondary</Button>
+			<Button size="compact" variant="secondary" active>Secondary active</Button>
+			<Button size="compact" variant="secondary" disabled>Secondary disabled</Button>
+		</div>
+		<div class="col">
+			<Button size="compact" variant="ghost">Ghost</Button>
+			<Button size="compact" variant="ghost" active>Ghost active</Button>
+			<Button size="compact" variant="ghost" disabled>Ghost disabled</Button>
+		</div>
+		<div class="col">
+			<Button size="compact" variant="overlay">Overlay</Button>
+			<Button size="compact" variant="overlay" active>Overlay active</Button>
+			<Button size="compact" variant="overlay" disabled>Overlay disabled</Button>
+		</div>
+		<div class="col">
+			<Button size="compact" variant="darkoverlay">Dark overlay</Button>
+			<Button size="compact" variant="darkoverlay" active>Dark overlay active</Button>
+			<Button size="compact" variant="darkoverlay" disabled>Dark overlay disabled</Button>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col">
+			<Button size="small">Strong</Button>
+			<Button size="small" active>Strong active</Button>
+			<Button size="small" disabled>Strong disabled</Button>
+		</div>
+		<div class="col">
+			<Button size="small"><Checkmark size={16} /></Button>
+			<Button size="small" active><Checkmark size={16} /></Button>
+			<Button size="small" disabled><Checkmark size={16} /></Button>
+		</div>
+		<div class="col">
+			<Button size="small"><Checkmark size={16} />Strong</Button>
+			<Button size="small" active><Checkmark size={16} />Strong active</Button>
+			<Button size="small" disabled><Checkmark size={16} />Strong disabled</Button>
+		</div>
+		<div class="col">
+			<Button size="small">Strong<ArrowRight size={16} /></Button>
+			<Button size="small" active>Strong active<ArrowRight size={16} /></Button>
+			<Button size="small" disabled>Strong disabled<ArrowRight size={16} /></Button>
+		</div>
+		<div class="col">
+			<Button size="small" variant="secondary">Secondary</Button>
+			<Button size="small" variant="secondary" active>Secondary active</Button>
+			<Button size="small" variant="secondary" disabled>Secondary disabled</Button>
+		</div>
+		<div class="col">
+			<Button size="small" variant="ghost">Ghost</Button>
+			<Button size="small" variant="ghost" active>Ghost active</Button>
+			<Button size="small" variant="ghost" disabled>Ghost disabled</Button>
+		</div>
+		<div class="col">
+			<Button size="small" variant="overlay">Overlay</Button>
+			<Button size="small" variant="overlay" active>Overlay active</Button>
+			<Button size="small" variant="overlay" disabled>Overlay disabled</Button>
+		</div>
+		<div class="col">
+			<Button size="small" variant="darkoverlay">Dark overlay</Button>
+			<Button size="small" variant="darkoverlay" active>Dark overlay active</Button>
+			<Button size="small" variant="darkoverlay" disabled>Dark overlay disabled</Button>
+		</div>
+	</div>
+</section>
+
+<style>
+	.row {
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		align-items: flex-start;
+		flex-wrap: wrap;
+		gap: 1rem;
+		padding-bottom: 5rem;
+	}
+
+	.col {
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: flex-start;
+		gap: 1rem;
+		border: 1px dashed var(--colors-ultra-high);
+		padding: 1rem;
+	}
+</style>
