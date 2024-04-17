78444f966a5fb4f7f9c0ebca69ddbbb557cf2191
Attila Gazso <230163+agazso@users.noreply.github.com>
2024-04-17 10:23:58 +0200

    fix: remove unused code

--- src/routes/csstest/+page.svelte
+++ src/routes/csstest/+page.svelte
@@ -1,14 +1,6 @@
 <script lang="ts">
-	import { onMount } from 'svelte'
 	import button from '$lib/components/ui/button.svelte?raw'
 	import Code from '$lib/components/custom/code.svelte'
-
-	let css: string | undefined
-
-	onMount(async () => {
-		const response = await fetch('/generated/css/ui/button.css')
-		css = await response.text()
-	})
 </script>
 
 <Code language="svelte" code={button} />
