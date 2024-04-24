2a86dfb608a35709e01fca66083749e5e4be8395
Attila Gazso <230163+agazso@users.noreply.github.com>
2024-04-24 15:10:23 +0200

    feat: button component page (#49)
    * wip: button component page
    
    * wip: add missing sections
    
    * wip: button component page
    
    * refactor: make button page
    
    * fix: formatting
    
    * feat: controls
    
    * fix: switch controls
    
    * fix: formatting
    
    * fix: revert select style to original
    
    * fix: responsive style with grid
    
    * fix: select inputs

---
+++ src/lib/components/custom/vertical-container.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte'
+
+	type Props = {
+		children: Snippet
+	}
+
+	const { children }: Props = $props()
+</script>
+
+<div class="vertical-container">
+	{#if children}
+		{@render children()}
+	{/if}
+</div>
+
+<style lang="postcss">
+	.vertical-container {
+		display: grid;
+		gap: 16px;
+		grid-template-columns: 1fr;
+	}
+	@media only screen and (min-device-width: 961px) {
+		.vertical-container {
+			grid-template-columns: 1fr 1fr;
+		}
+	}
+</style>
