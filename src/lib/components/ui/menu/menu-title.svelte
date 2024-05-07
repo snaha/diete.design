43efa0002861c13024b2764f164e54132976a394
Attila Gazso <230163+agazso@users.noreply.github.com>
2024-05-07 10:27:23 +0200

    fix: menu title clickable area
    Fixes #79

--- src/lib/components/ui/menu/menu-title.svelte
+++ src/lib/components/ui/menu/menu-title.svelte
@@ -84,8 +84,12 @@
 			display: flex;
 			flex-direction: row;
 			align-items: center;
-			justify-content: space-between;
-			gap: 0.5rem;
+			justify-content: stretch;
+		}
+		.title {
+			display: flex;
+			flex-grow: 1;
+			padding-right: 0.5rem;
 		}
 		.icon {
 			display: flex;
