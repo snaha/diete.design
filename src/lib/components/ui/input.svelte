561886e03da4c076aed7d61621499f4716f3256d
hlavkat <137988631+hlavkat@users.noreply.github.com>
2024-05-09 10:41:06 +0200

    feat: update Input component to Diete v5

--- src/lib/components/ui/input.svelte
+++ src/lib/components/ui/input.svelte
@@ -1,50 +1,78 @@
 <script lang="ts">
+	import type { Snippet } from 'svelte'
 	import type { HTMLInputAttributes } from 'svelte/elements'
+	import { WarningAltFilled, Information } from 'carbon-icons-svelte'
 	type Layout = 'vertical' | 'horizontal'
 	type Dimension = 'default' | 'large' | 'compact' | 'small'
 	interface Props extends HTMLInputAttributes {
+		label?: string
 		labelFor?: string
 		dimension?: Dimension
 		layout?: Layout
 		unit?: string
+		error?: Snippet
+		hover?: boolean
+		active?: boolean
+		focus?: boolean
 	}
 	let {
+		label,
 		labelFor = Math.random().toString(16),
 		placeholder,
 		value = $bindable(),
 		dimension = 'default',
 		layout = 'vertical',
 		unit,
+		error,
+		hover,
+		active,
+		focus,
 		type,
 		class: className = '',
+		children,
 		...restProps
 	}: Props = $props()
 </script>
 
-{#if layout == 'vertical'}
-	<div class="root {layout} {dimension} {className}">
-		<input id={labelFor} bind:value {placeholder} {type} {...restProps} />
-		<label class="label" for={labelFor}>
+<div class="root {layout} {dimension} {className}">
+	<label class="label" for={labelFor}>
+		{label}
+	</label>
+	{#if children && layout === 'horizontal'}
+		<div class="helper-button">
+			<Information size={dimension === 'small' ? 16 : 24} />
+		</div>
+	{/if}
+	<div class="wrapper">
+		<input
+			id={labelFor}
+			class:active
+			class:hover
+			class:focus
+			class:error
+			bind:value
 			{placeholder}
-		</label>
-		{#if unit}
+			{type}
+			{...restProps}
+		/>
+		{#if unit && !error}
 			<label class="unit" for={labelFor}>{unit}</label>
 		{/if}
-		<div class="helper-text">
-			<slot />
-		</div>
-	</div>
-{:else}
-	<div class="root {layout} {dimension} {className}">
-		<input id={labelFor} bind:value {placeholder} {type} {...restProps} />
-		<label class="label" for={labelFor}>
-			<slot />
-		</label>
-		{#if unit}
-			<label for={labelFor} class="unit">{unit}</label>
+		{#if error}
+			<label class="error-icon" for={labelFor}>
+				<WarningAltFilled size={dimension === 'small' ? 16 : 24} />
+			</label>
+			<div class="error-message">
+				{@render error()}
+			</div>
 		{/if}
 	</div>
-{/if}
+	{#if children && layout === 'vertical'}
+		<div class="helper-text">
+			{@render children()}
+		</div>
+	{/if}
+</div>
 
 <style lang="postcss">
 	input[type='number']::-webkit-inner-spin-button {
@@ -52,345 +80,226 @@
 	}
 	.vertical {
 		&.root {
-			position: relative;
-			color: var(--colors-ultra-high);
-			font-family: var(--font-family-sans-serif);
 			display: flex;
 			flex-direction: column;
-			gap: 0.5rem;
-		}
-		input {
-			background: var(--colors-low);
-			border: 1px solid var(--colors-low);
-			border-radius: 0.25rem;
-			&:focus {
-				border: 1px solid var(--colors-high);
-				background: var(--colors-base);
-				color: var(--colors-top);
-				outline: none;
-				& + .label {
-					background: var(--colors-base);
-					font-size: var(--font-size-small);
-					line-height: var(--line-height-small);
-				}
-				& ~ .unit {
-					opacity: 1;
-				}
-			}
-			&:not(:placeholder-shown) {
-				& + .label {
-					font-size: var(--font-size-small);
-					line-height: var(--line-height-small);
-				}
-				& ~ .unit {
-					opacity: 1;
-				}
-			}
-			&:not(:focus) {
-				color: var(--colors-ultra-high);
-			}
-			&:disabled {
-				opacity: 0.25;
-				cursor: not-allowed;
-				& + .label {
-					background: transparent;
-					opacity: 0.25;
-					cursor: not-allowed;
-				}
-				& ~ .unit {
-					opacity: 0.25;
-					cursor: not-allowed;
-				}
-			}
-		}
-
-		input::placeholder {
-			text-align: center;
-			color: transparent;
-			user-select: none;
-		}
-		.label {
-			position: absolute;
-			left: 0.75rem;
-			user-select: none;
-			transform-origin: left center;
-			transition: transform 0.25s;
-			border: none;
-			color: var(--colors-high);
-			background: var(--colors-low);
-			cursor: text;
-		}
-		.unit {
-			position: absolute;
-			user-select: none;
-			opacity: 0;
-			transition: opacity 0.25s;
-			cursor: text;
-		}
-		&.default {
-			.label {
-				font-size: var(--font-size);
-				line-height: var(--line-height);
-				letter-spacing: var(--letter-spacing);
-				top: 1.5rem;
-			}
-			input {
-				font-size: var(--font-size);
-				line-height: var(--line-height);
-				letter-spacing: var(--letter-spacing);
-				padding: 1.5rem 0.75rem;
-				&:focus {
-					padding: 2.25rem 0.75rem 0.75rem;
-					& + .label {
-						transform: translateY(-0.75rem);
-					}
-				}
-				&:not(:placeholder-shown) {
-					padding: 2.25rem 0.75rem 0.75rem;
-					& + .label {
-						transform: translateY(-0.75rem);
-					}
-				}
-			}
-			.unit {
-				top: 0.75rem;
-				right: 0.75rem;
-				padding: 0.75rem;
-				font-size: var(--font-size);
-				line-height: var(--line-height);
-				letter-spacing: var(--letter-spacing);
-			}
-		}
-		&.large {
-			.label {
-				font-size: var(--font-size-large);
-				line-height: var(--line-height-large);
-				letter-spacing: var(--letter-spacing-large);
-				top: 1.5rem;
-			}
-			input {
-				font-size: var(--font-size-large);
-				line-height: var(--line-height-large);
-				letter-spacing: var(--letter-spacing-large);
-				padding: 1.5rem 0.75rem;
-				&:focus {
-					padding: 2.25rem 0.75rem 0.75rem;
-					& + .label {
-						transform: translateY(-0.75rem);
-					}
-				}
-				&:not(:placeholder-shown) {
-					padding: 2.25rem 0.75rem 0.75rem;
-					& + .label {
-						transform: translateY(-0.75rem);
-					}
-				}
-			}
-			.unit {
-				top: 0.75rem;
-				right: 0.75rem;
-				padding: 0.75rem;
-				font-size: var(--font-size-large);
-				line-height: var(--line-height-large);
-				letter-spacing: var(--letter-spacing-large);
-			}
-		}
-		&.compact {
-			.label {
-				font-size: var(--font-size);
-				line-height: var(--line-height);
-				letter-spacing: var(--letter-spacing);
-				top: 1rem;
-			}
-			input {
-				font-size: var(--font-size);
-				line-height: var(--line-height);
-				letter-spacing: var(--letter-spacing);
-				padding: 1rem 0.75rem;
-				&:focus {
-					padding: 1.5rem 0.75rem 0.5rem;
-					& + label {
-						transform: translateY(-0.5rem);
-					}
-				}
-				&:not(:placeholder-shown) {
-					padding: 1.5rem 0.75rem 0.5rem;
-					& + .label {
-						transform: translateY(-0.5rem);
-					}
-				}
-			}
-			.unit {
-				top: 0.5rem;
-				right: 0.5rem;
-				padding: 0.5rem;
-				font-size: var(--font-size);
-				line-height: var(--line-height);
-				letter-spacing: var(--letter-spacing);
-			}
-		}
-		&.small {
-			.label {
-				font-size: var(--font-size-small);
-				line-height: var(--line-height-small);
-				letter-spacing: var(--letter-spacing-small);
-				top: 1rem;
-			}
-			input {
-				font-size: var(--font-size-small);
-				line-height: var(--line-height-small);
-				letter-spacing: var(--letter-spacing-small);
-				padding: 1rem 0.75rem;
-				&:focus {
-					padding: 1.5rem 0.75rem 0.5rem;
-					& + .label {
-						transform: translateY(-0.5rem);
-					}
-				}
-				&:not(:placeholder-shown) {
-					padding: 1.5rem 0.75rem 0.5rem;
-					& + .label {
-						transform: translateY(-0.5rem);
-					}
-				}
-			}
-			.unit {
-				top: 0.5rem;
-				right: 0.5rem;
-				padding: 0.5rem;
-				font-size: var(--font-size-small);
-				line-height: var(--line-height-small);
-				letter-spacing: var(--letter-spacing-small);
-			}
-		}
-		.helper-text {
-			font-size: var(--font-size-small);
-			line-height: var(--line-height-small);
-			letter-spacing: var(--letter-spacing-small);
-			padding: 0 0.75rem;
 		}
 	}
 	.horizontal {
 		&.root {
-			color: var(--colors-ultra-high);
-			font-family: var(--font-family-sans-serif);
-			position: relative;
 			display: flex;
-			flex-direction: row-reverse;
+			flex-direction: row;
 			align-items: center;
-			gap: 0.5rem;
 		}
+	}
+	.root {
+		color: var(--colors-ultra-high);
+		font-family: var(--font-family-sans-serif);
+		gap: 0.5rem;
+	}
+	.label {
+		display: flex;
+		flex-direction: column;
+		color: var(--colors-ultra-high);
+		cursor: pointer;
+		width: fit-content;
+	}
+	.helper-button {
+		display: flex;
+		align-items: center;
+		cursor: pointer;
+	}
+	.wrapper {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		gap: 0.25rem;
 		input {
-			background: var(--colors-low);
-			border: 1px solid var(--colors-low);
+			border: 1px solid var(--colors-ultra-high);
 			border-radius: 0.25rem;
+			background: transparent;
+			color: var(--colors-ultra-high);
 			&::placeholder {
-				color: var(--colors-high);
-			}
-			&:focus {
-				border: 1px solid var(--colors-high);
-				background: var(--colors-base);
-				color: var(--colors-top);
-				outline: none;
-				& ~ .unit {
-					opacity: 1;
-				}
-			}
-			&:not(:placeholder-shown) ~ .unit {
-				opacity: 1;
-			}
-			&:not(:focus) {
 				color: var(--colors-ultra-high);
+				opacity: 0.5;
 			}
 			&:disabled {
 				opacity: 0.25;
 				cursor: not-allowed;
-				& + .label {
-					background: transparent;
-					opacity: 0.25;
-					cursor: not-allowed;
-				}
-				& ~ .unit {
+				& ~ .unit,
+				& ~ .error-icon {
 					opacity: 0.25;
 					cursor: not-allowed;
 				}
 			}
+			&:focus:not(:disabled),
+			&:focus-visible:not(:disabled),
+			&.focus:not(:disabled) {
+				outline: 4px solid var(--colors-top);
+				outline-offset: -4px;
+				background: var(--colors-base);
+				color: var(--colors-top);
+				& ~ .unit {
+					color: var(--colors-top);
+				}
+			}
+			&:active:not(:disabled),
+			&.active:not(:disabled) {
+				outline: none;
+			}
+			&:hover:not(:disabled),
+			&.hover:not(:disabled),
+			&:active:not(:disabled),
+			&.active:not(:disabled) {
+				border: 1px solid var(--colors-top);
+				background: var(--colors-base);
+				color: var(--colors-top);
+			}
+			&.error:not(:disabled) {
+				outline: 2px solid var(--colors-top);
+				outline-offset: -2px;
+			}
 		}
+		.unit {
+			position: absolute;
+			cursor: text;
+		}
+		.error-icon {
+			position: absolute;
+			color: var(--colors-top);
+			cursor: text;
+		}
+	}
+	.default {
+		.label {
+			font-size: var(--font-size);
+			line-height: var(--line-height);
+			letter-spacing: var(--letter-spacing);
+		}
+		.helper-button {
+			padding: 0.75rem;
+		}
+		input {
+			font-size: var(--font-size);
+			line-height: var(--line-height);
+			letter-spacing: var(--letter-spacing);
+			padding: 0.75rem;
+		}
+		.unit {
+			top: 0.75rem;
+			right: 0.75rem;
+			font-size: var(--font-size);
+			line-height: var(--line-height);
+			letter-spacing: var(--letter-spacing);
+		}
+		.error-icon {
+			top: 0.75rem;
+			right: 0.75rem;
+		}
+	}
+	.large {
+		.label {
+			font-size: var(--font-size-large);
+			line-height: var(--line-height-large);
+			letter-spacing: var(--letter-spacing-large);
+		}
+		.helper-button {
+			padding: 0.75rem;
+		}
+		input {
+			font-size: var(--font-size-large);
+			line-height: var(--line-height-large);
+			letter-spacing: var(--letter-spacing-large);
+			padding: 0.75rem;
+		}
+		.unit {
+			top: 0.75rem;
+			right: 0.75rem;
+			font-size: var(--font-size-large);
+			line-height: var(--line-height-large);
+			letter-spacing: var(--letter-spacing-large);
+		}
+		.error-icon {
+			top: 0.75rem;
+			right: 0.75rem;
+		}
+		.error-message {
+			font-size: var(--font-size-large);
+			line-height: var(--line-height-large);
+			letter-spacing: var(--letter-spacing-large);
+		}
+	}
+	.compact {
+		.label {
+			font-size: var(--font-size);
+			line-height: var(--line-height);
+			letter-spacing: var(--letter-spacing);
+		}
+		.helper-button {
+			padding: 0.5rem;
+		}
+		input {
+			font-size: var(--font-size);
+			line-height: var(--line-height);
+			letter-spacing: var(--letter-spacing);
+			padding: 0.5rem;
+		}
+		.unit {
+			top: 0.5rem;
+			right: 0.5rem;
+			font-size: var(--font-size);
+			line-height: var(--line-height);
+			letter-spacing: var(--letter-spacing);
+		}
+		.error-icon {
+			top: 0.5rem;
+			right: 0.5rem;
+		}
+	}
+	.small {
 		.label {
 			font-size: var(--font-size-small);
 			line-height: var(--line-height-small);
 			letter-spacing: var(--letter-spacing-small);
-			user-select: none;
-			transform-origin: left center;
-			color: var(--colors-high);
-			cursor: pointer;
+		}
+		.helper-button {
+			padding: 0.5rem;
+		}
+		input {
+			font-size: var(--font-size-small);
+			line-height: var(--line-height-small);
+			letter-spacing: var(--letter-spacing-small);
+			padding: 0.5rem;
 		}
 		.unit {
-			position: absolute;
-			user-select: none;
-			opacity: 0;
-			transition: opacity 0.25s;
-			cursor: text;
+			top: 0.5rem;
+			right: 0.5rem;
+			font-size: var(--font-size-small);
+			line-height: var(--line-height-small);
+			letter-spacing: var(--letter-spacing-small);
 		}
-		&.default {
-			input {
-				font-size: var(--font-size);
-				line-height: var(--line-height);
-				letter-spacing: var(--letter-spacing);
-				padding: 0.75rem;
-			}
-			.unit {
-				top: 0.75rem;
-				right: 0.75rem;
-				font-size: var(--font-size);
-				line-height: var(--line-height);
-				letter-spacing: var(--letter-spacing);
-			}
+		.error-icon {
+			top: 0.5rem;
+			right: 0.5rem;
 		}
-		&.large {
-			input {
-				font-size: var(--font-size-large);
-				line-height: var(--line-height-large);
-				letter-spacing: var(--letter-spacing-large);
-				padding: 0.75rem;
-			}
-			.unit {
-				top: 0.75rem;
-				right: 0.75rem;
-				font-size: var(--font-size-large);
-				line-height: var(--line-height-large);
-				letter-spacing: var(--letter-spacing-large);
-			}
+		.error-message {
+			font-size: var(--font-size-small);
+			line-height: var(--line-height-small);
+			letter-spacing: var(--letter-spacing-small);
 		}
-		&.compact {
-			input {
-				font-size: var(--font-size);
-				line-height: var(--line-height);
-				letter-spacing: var(--letter-spacing);
-				padding: 0.5rem;
-			}
-			.unit {
-				top: 0.5rem;
-				right: 0.5rem;
-				font-size: var(--font-size);
-				line-height: var(--line-height);
-				letter-spacing: var(--letter-spacing);
-			}
-		}
-		&.small {
-			input {
-				font-size: var(--font-size-small);
-				line-height: var(--line-height-small);
-				letter-spacing: var(--letter-spacing-small);
-				padding: 0.5rem;
-			}
-			.unit {
-				top: 0.5rem;
-				right: 0.5rem;
-				font-size: var(--font-size-small);
-				line-height: var(--line-height-small);
-				letter-spacing: var(--letter-spacing-small);
-			}
-		}
+	}
+	.error-message {
+		border: 1px solid var(--colors-top);
+		border-radius: 0.25rem;
+		background: var(--colors-top);
+		color: var(--colors-base);
+		padding: 0.25rem 0.5rem;
+		font-size: var(--font-size);
+		line-height: var(--line-height);
+		letter-spacing: var(--letter-spacing);
+	}
+	.helper-text {
+		font-size: var(--font-size-small);
+		line-height: var(--line-height-small);
+		letter-spacing: var(--letter-spacing-small);
 	}
 </style>
