c0817576117c425eff07050f76fd4a328a2d313f
hlavkat <137988631+hlavkat@users.noreply.github.com>
2024-05-13 08:13:33 +0200

    fix: tabindex should come from props

--- src/lib/components/ui/menu/menu-item.svelte
+++ src/lib/components/ui/menu/menu-item.svelte
@@ -3,7 +3,7 @@
 	import type { MenuStore } from './menu-store.svelte'
 	import Button, { type Props } from '../button.svelte'
 
-	let { dimension, disabled, ...restProps }: Props = $props()
+	let { dimension, disabled, tabindex, ...restProps }: Props = $props()
 
 	const store = getContext<MenuStore>('menu-store')
 </script>
@@ -12,6 +12,6 @@
 	variant="ghost"
 	dimension={dimension ?? store.size}
 	disabled={disabled ?? store.disabled}
-	tabindex={disabled || store.disabled || !store.open ? -1 : 0}
+	tabindex={disabled || store.disabled || !store.open ? -1 : tabindex}
 	{...restProps}
 />
