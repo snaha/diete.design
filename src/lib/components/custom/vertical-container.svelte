b35ba856dc27afb0cfe859a1255c1ca4c80c0997
Attila Gazso <230163+agazso@users.noreply.github.com>
2024-05-22 12:37:20 +0200

    fix: responsive layout

--- src/lib/components/custom/vertical-container.svelte
+++ src/lib/components/custom/vertical-container.svelte
@@ -16,24 +16,14 @@
 
 <style lang="postcss">
 	.vertical-container {
-		--grid-layout-gap: 16px;
-		--grid-column-count: 2; /* This gets overridden by an inline style. */
-  		--grid-item--min-width: 320px; /* This gets overridden by an inline style. */
-
-		/**
-		* Calculated values.
-		*/
-		--gap-count: calc(var(--grid-column-count) - 1);
-		--total-gap-width: calc(var(--gap-count) * var(--grid-layout-gap));
-		--grid-item--max-width: calc((100% - var(--total-gap-width)) / var(--grid-column-count));
-
-		display: grid;
-		grid-template-columns: repeat(auto-fill, minmax(max(var(--grid-item--min-width), var(--grid-item--max-width)), 1fr));
-		grid-gap: var(--grid-layout-gap);
-
-		/* display: grid;
-		grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+		display: flex;
+		flex-wrap: wrap;
 		gap: 16px;
-		max-width: inherit; */
+
+		:global(section) {
+			flex: 1;
+			box-sizing: border-box;
+			min-width: 320px;
+		}
 	}
 </style>
