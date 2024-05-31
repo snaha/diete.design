34536704baee954aec65a56e5175fe537af1f367
hlavkat <137988631+hlavkat@users.noreply.github.com>
2024-05-31 10:37:14 +0200

    feat: tooltip component page

---
+++ src/routes/components/tooltip/+page.svelte
@@ -0,0 +1,141 @@
+<script lang="ts">
+	import tooltip from '$lib/components/ui/tooltip.svelte?raw'
+	import Code from '$lib/components/custom/code.svelte'
+	import TabBar from '$lib/components/custom/tab-bar/tab-bar.svelte'
+	import TabContent from '$lib/components/custom/tab-bar/tab-content.svelte'
+	import { onMount } from 'svelte'
+	import Typography from '$lib/components/ui/typography.svelte'
+	import Select from '$lib/components/ui/select/select.svelte'
+	import Option from '$lib/components/ui/select/option.svelte'
+	import ComponentTemplate from '$lib/components/custom/component-template.svelte'
+	import Tooltip from '$lib/components/ui/tooltip.svelte'
+	import { Information } from 'carbon-icons-svelte'
+	import Switch from '$lib/components/ui/switch.svelte'
+	import Input from '$lib/components/ui/input.svelte'
+
+	let css: string = $state('Loading...')
+	type Position = 'top' | 'bottom' | 'left' | 'right'
+	let position: Position = $state('top')
+	let large: boolean = $state(false)
+	let tooltipText: string = $state('This is some tooltip text')
+
+	// Svelte compiler breaks when it finds closing script tag, hence the need to make the template literal to have two parts
+	let useCode = $derived(
+		`<script lang="ts">
+	import Tooltip from '$lib/components/ui/tooltip.svelte'
+</script` +
+			`>
+{#snippet helperText()}
+	${tooltipText}
+{/snippet}	
+<Tooltip position=${position} {helperText}${large ? ` large` : ''}><Information size={24} /></Tooltip>
+`,
+	)
+
+	onMount(async () => {
+		const response = await fetch('/generated/css/ui/tooltip.css')
+		css = await response.text()
+	})
+</script>
+
+{#snippet description()}
+	<Typography>
+		Tooltip is available in default and large version.
+		<br />
+		<br />
+		The tooltip can be displayed at the top, bottom, left or right.
+		<br />
+		<br />
+		The tooltip will move when there is no space.
+		<br />
+		<br />
+	</Typography>
+{/snippet}
+
+{#snippet helperText()}
+	{tooltipText}
+{/snippet}
+
+{#snippet examples()}
+	<div class="example-row">
+		<Typography variant="small" bold>1. Tooltip on top</Typography>
+		<Tooltip position="top" {helperText}><Information size={24} /></Tooltip>
+	</div>
+	<div class="example-row">
+		<Typography variant="small" bold>1. Tooltip on bottom</Typography>
+		<Tooltip position="bottom" {helperText}><Information size={24} /></Tooltip>
+	</div>
+	<div class="example-row">
+		<Typography variant="small" bold>1. Tooltip on left</Typography>
+		<Tooltip position="left" {helperText}><Information size={24} /></Tooltip>
+	</div>
+	<div class="example-row">
+		<Typography variant="small" bold>1. Tooltip on right</Typography>
+		<Tooltip position="right" {helperText}><Information size={24} /></Tooltip>
+	</div>
+{/snippet}
+
+{#snippet controls()}
+	<Select bind:value={position} label="Tooltip position">
+		<Option value="top">Top</Option>
+		<Option value="bottom">Bottom</Option>
+		<Option value="left">Left</Option>
+		<Option value="right">Right</Option>
+	</Select>
+	<Input label="Tooltip text" bind:value={tooltipText} />
+	<Switch label="Large tooltip" bind:checked={large} />
+{/snippet}
+
+{#snippet preview()}
+	<TabBar dimension="small">
+		<TabContent value="Preview">
+			<div class="preview-tabs preview-tab">
+				<Tooltip {position} {helperText} large={large ? large : undefined}
+					><Information size={24} /></Tooltip
+				>
+			</div>
+		</TabContent>
+		<TabContent value="Svelte"
+			><Code language="svelte" code={useCode} class="preview-tab" /></TabContent
+		>
+	</TabBar>
+{/snippet}
+
+{#snippet implement()}
+	<TabBar dimension="small">
+		<TabContent value="Svelte"><Code language="svelte" code={tooltip} /></TabContent>
+		<TabContent value="CSS"><Code language="css" code={css} /></TabContent>
+	</TabBar>
+{/snippet}
+
+<ComponentTemplate
+	name="Tooltip"
+	tagline="Hover or click on the trigger and display the tooltip"
+	{description}
+	{examples}
+	{controls}
+	{preview}
+	{implement}
+/>
+
+<style lang="postcss">
+	.preview-tabs {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		background-color: var(--colors-ultra-low);
+	}
+	:global(.preview-tab) {
+		height: 288px;
+	}
+	.example-row {
+		display: flex;
+		flex: 1;
+		flex-wrap: wrap;
+		align-items: center;
+		gap: 16px;
+		margin-top: 0px;
+		margin-bottom: 0px;
+	}
+</style>
