0407912a23393db6e3fcf24f8dbe6b46b2a05ee9
Attila Gazso <230163+agazso@users.noreply.github.com>
2024-06-05 16:49:19 +0200

    feat: colors page

---
+++ src/routes/colors/+page.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import { browser } from '$app/environment'
+	import Code from '$lib/components/custom/code.svelte'
+	import { onMount } from 'svelte'
+
+	const getCSSProperty = (name: string) => {
+		if (!browser) {
+			return ''
+		}
+		return document?.documentElement?.style?.getPropertyValue(name)
+	}
+	let css = $state('')
+
+	onMount(() => {
+		setInterval(() => getColors(), 500)
+	})
+
+	const getColors = () => {
+		css = `
+	--colors-top: ${getCSSProperty('--colors-top')};
+	--colors-ultra-high: ${getCSSProperty('--colors-ultra-high')};
+	--colors-high: ${getCSSProperty('--colors-high')};
+	--colors-low: ${getCSSProperty('--colors-low')};
+	--colors-ultra-low: ${getCSSProperty('--colors-ultra-low')};
+	--colors-base: ${getCSSProperty('--colors-base')};
+
+	--colors-dark-top: ${getCSSProperty('--colors-dark-top')};
+	--colors-dark-ultra-high: ${getCSSProperty('--colors-dark-ultra-high')};
+	--colors-dark-high: ${getCSSProperty('--colors-dark-high')};
+	--colors-dark-low: ${getCSSProperty('--colors-dark-low')};
+	--colors-dark-ultra-low: ${getCSSProperty('--colors-dark-ultra-low')};
+	--colors-dark-base: ${getCSSProperty('--colors-dark-base')};
+
+	--colors-light-top: ${getCSSProperty('--colors-light-top')};
+	--colors-light-ultra-high: ${getCSSProperty('--colors-light-ultra-high')};
+	--colors-light-high: ${getCSSProperty('--colors-light-high')};
+	--colors-light-low: ${getCSSProperty('--colors-light-low')};
+	--colors-light-ultra-low: ${getCSSProperty('--colors-light-ultra-low')};
+	--colors-light-base: ${getCSSProperty('--colors-light-base')};
+
+	--colors-dark-overlay: ${getCSSProperty('--colors-dark-overlay')};
+	--colors-light-overlay: ${getCSSProperty('--colors-light-overlay')};
+`
+	}
+</script>
+
+<Code class="code-max-height" language="css" code={css} />
+
+<style>
+	:global(.code-max-height) {
+		margin-bottom: var(--double-padding);
+		max-height: 80vh !important;
+	}
+</style>
