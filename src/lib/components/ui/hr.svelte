e823f63feaf4feed63836d2313902e57d15a95f9
hlavkat <137988631+hlavkat@users.noreply.github.com>
2024-06-10 13:54:39 +0200

    feat: horizontal rule component

---
+++ src/lib/components/ui/hr.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements'
+
+	interface Props extends HTMLAttributes<HTMLHRElement> {
+		class?: string
+	}
+	let { class: className = '', ...restProps }: Props = $props()
+</script>
+
+<hr class={className} {...restProps} />
+
+<style>
+	hr {
+		appearance: none;
+		margin-top: var(--double-padding);
+		margin-bottom: var(--double-padding);
+		border-width: 1px;
+		border-style: solid;
+		width: 100%;
+		color: var(--colors-low);
+	}
+</style>
