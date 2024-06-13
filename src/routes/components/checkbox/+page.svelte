c95c07ac88afe7ebecfe82a78b4cf56f3f87b65f
hlavkat <137988631+hlavkat@users.noreply.github.com>
2024-06-13 09:50:58 +0200

    checkbox component page

--- src/routes/components/checkbox/+page.svelte
+++ src/routes/components/checkbox/+page.svelte
@@ -10,6 +10,7 @@
 	import Option from '$lib/components/ui/select/option.svelte'
 	import ComponentTemplate from '$lib/components/custom/component-template.svelte'
 	import Input from '$lib/components/ui/input.svelte'
+	import CodeComponentTemplate from '$lib/components/custom/code-component-template.svelte'
 
 	type Dimension = 'default' | 'large' | 'compact' | 'small'
 
@@ -80,11 +81,16 @@ import Checkbox from '$lib/components/ui/checkbox.svelte'
 	<Checkbox {dimension} {label} />
 {/snippet}
 
+{#snippet extraSvelte()}
+	<TabContent value="checkbox"><Code language="svelte" code={checkbox} /></TabContent>
+{/snippet}
+
+{#snippet extraCss()}
+	<TabContent value="checkbox"><Code language="css" code={css} /></TabContent>
+{/snippet}
+
 {#snippet implement()}
-	<TabBar dimension="small">
-		<TabContent value="Svelte"><Code language="svelte" code={checkbox} /></TabContent>
-		<TabContent value="CSS"><Code language="css" code={css} /></TabContent>
-	</TabBar>
+	<CodeComponentTemplate {extraSvelte} {extraCss} />
 {/snippet}
 
 <ComponentTemplate
