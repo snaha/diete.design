591e616bd7120ba04cd359c1ea75372202d9f2da
hlavkat <137988631+hlavkat@users.noreply.github.com>
2024-06-13 10:05:48 +0200

    menu component page

--- src/routes/components/menu/+page.svelte
+++ src/routes/components/menu/+page.svelte
@@ -3,7 +3,6 @@
 	import menuItem from '$lib/components/ui/menu/menu-item.svelte?raw'
 	import menuStore from '$lib/components/ui/menu/menu-store.svelte.ts?raw'
 	import Code from '$lib/components/custom/code.svelte'
-	import TabBar from '$lib/components/custom/tab-bar/tab-bar.svelte'
 	import TabContent from '$lib/components/custom/tab-bar/tab-content.svelte'
 	import { onMount } from 'svelte'
 	import Typography from '$lib/components/ui/typography.svelte'
@@ -13,6 +12,7 @@
 	import Input from '$lib/components/ui/input.svelte'
 	import MenuTitle from '$lib/components/ui/menu/menu-title.svelte'
 	import MenuItem from '$lib/components/ui/menu/menu-item.svelte'
+	import CodeComponentTemplate from '$lib/components/custom/code-component-template.svelte'
 
 	type Dimension = 'default' | 'large' | 'compact' | 'small'
 
@@ -109,21 +109,18 @@
 	</MenuTitle>
 {/snippet}
 
+{#snippet extraSvelte()}
+	<TabContent value="menu-title"><Code language="svelte" code={menuTitle} /></TabContent>
+	<TabContent value="menu-item"><Code language="svelte" code={menuItem} /></TabContent>
+	<TabContent value="menu-store"><Code language="svelte" code={menuStore} /></TabContent>
+{/snippet}
+
+{#snippet extraCss()}
+	<TabContent value="menu-title"><Code language="css" code={menuTitleCss} /></TabContent>
+{/snippet}
+
 {#snippet implement()}
-	<TabBar dimension="small">
-		<TabContent value="Svelte">
-			<TabBar dimension="small">
-				<TabContent value="menu-title"><Code language="svelte" code={menuTitle} /></TabContent>
-				<TabContent value="menu-item"><Code language="svelte" code={menuItem} /></TabContent>
-				<TabContent value="menu-store"><Code language="svelte" code={menuStore} /></TabContent>
-			</TabBar>
-		</TabContent>
-		<TabContent value="CSS">
-			<TabBar dimension="small">
-				<TabContent value="menu-title"><Code language="css" code={menuTitleCss} /></TabContent>
-			</TabBar>
-		</TabContent>
-	</TabBar>
+	<CodeComponentTemplate {extraSvelte} {extraCss} />
 {/snippet}
 
 <ComponentTemplate
