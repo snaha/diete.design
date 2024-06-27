d06b79e4d8911777fda173bb62c2a34cba454d67
hlavkat <137988631+hlavkat@users.noreply.github.com>
2024-06-27 08:25:54 +0200

    fix: loader color props

--- src/lib/components/ui/loader.svelte
+++ src/lib/components/ui/loader.svelte
@@ -1,21 +1,32 @@
 <script lang="ts">
 	import type { HTMLAttributes } from 'svelte/elements'
-
+	type Color = 'high' | 'low'
 	type Dimension = 'default' | 'large' | 'compact' | 'small'
 	type Props = {
 		dimension?: Dimension
+		color?: Color
 	}
-	let { dimension = 'default', ...restProps }: Props & HTMLAttributes<HTMLDivElement> = $props()
+	let {
+		dimension = 'default',
+		color = 'high',
+		...restProps
+	}: Props & HTMLAttributes<HTMLDivElement> = $props()
 </script>
 
-<div class="loader {dimension}" {...restProps}></div>
+<div class="loader {dimension} {color}" {...restProps}></div>
 
 <style lang="postcss">
 	.loader {
 		animation: spin 1s linear infinite;
-		border: 2px solid var(--loader-color, var(--colors-ultra-high));
-		border-top: 2px solid transparent;
 		border-radius: 50%;
+		&.high {
+			border: 2px solid var(--colors-ultra-high);
+			border-top: 2px solid transparent;
+		}
+		&.low {
+			border: 2px solid var(--colors-ultra-low);
+			border-top: 2px solid transparent;
+		}
 		&.default,
 		&.compact,
 		&.small {
