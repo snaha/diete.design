551fda17df7bf66824e16ac93627272a8462214c
hlavkat <hlavkat@gmail.com>
2024-07-11 13:19:51 +0200

    feat: automatic formating

--- src/lib/components/ui/input/number-input.svelte
+++ src/lib/components/ui/input/number-input.svelte
@@ -3,22 +3,110 @@
 	import Button from '../button.svelte'
 	import Input, { type Props } from './input.svelte'
 	import type { HTMLInputAttributes } from 'svelte/elements'
+	interface CustomProps extends Props {
+		formatter?: Intl.NumberFormat
+	}
 	let {
+		formatter = new Intl.NumberFormat(undefined, { maximumFractionDigits: 12 }),
 		value = $bindable(0),
 		dimension,
 		disabled,
 		...restProps
-	}: Props & Omit<HTMLInputAttributes, 'type'> = $props()
+	}: CustomProps & Omit<HTMLInputAttributes, 'type'> = $props()
 	let size: 16 | 24 | 32 = $derived(dimension === 'large' ? 32 : dimension === 'small' ? 16 : 24)
+
+	let stringValue = $state(numberToLocalString(value, formatter))
+
+	function numberToLocalString(value: number, formatter: Intl.NumberFormat): string {
+		return formatter.format(value)
+	}
+
+	function localeStringToNumber(
+		numberString: string,
+		formatter: Intl.NumberFormat,
+	): number | undefined {
+		const groupSeparator = formatter.format(11111).replace(/\p{Number}/gu, '')
+		const decimalSeparator = formatter.format(1.1).replace(/\p{Number}/gu, '')
+
+		// Replace the group separator and decimal separator with appropriate characters
+		const sanitizedNumberString = numberString
+			.split(groupSeparator)
+			.join('')
+			.replace(decimalSeparator, '.')
+		const regex = /^-?([0-9]+(.[0-9]+)?|.[0-9]+)$/
+		if (!regex.test(sanitizedNumberString)) {
+			console.error('Invalid number format')
+			return undefined
+		}
+		const parsedValue = parseFloat(sanitizedNumberString)
+		if (Number.isNaN(parsedValue)) {
+			return undefined
+		} else {
+			return parsedValue
+		}
+	}
+	let cursorPosition: number | null = 0
+	let distanceFromEnd = 0
+	function updateCursorPosition() {
+		const input = document.querySelector('.input input') as HTMLInputElement
+		let newCursorPosition = () => {
+			let position = input.value.length - distanceFromEnd
+			if (position < 0) {
+				position = 0
+			}
+			return position
+		}
+		input?.setSelectionRange(newCursorPosition(), newCursorPosition())
+	}
+	$inspect(typeof value, '-', value)
 </script>
 
 {#snippet buttons()}
-	<Button {dimension} {disabled} variant="secondary" onclick={() => (value -= 1)}>
+	<Button
+		{dimension}
+		{disabled}
+		variant="secondary"
+		onclick={() => {
+			value -= 1
+			stringValue = numberToLocalString(value, formatter)
+		}}
+	>
 		<Subtract {size} />
 	</Button>
-	<Button {dimension} {disabled} variant="secondary" onclick={() => (value += 1)}>
+	<Button
+		{dimension}
+		{disabled}
+		variant="secondary"
+		onclick={() => {
+			value += 1
+			stringValue = numberToLocalString(value, formatter)
+		}}
+	>
 		<Add {size} />
 	</Button>
 {/snippet}
 
-<Input bind:value {dimension} {disabled} {buttons} {...restProps} type="number" />
+<Input
+	class="input"
+	value={stringValue}
+	oninput={(e) => {
+		stringValue = e.currentTarget.value
+		cursorPosition = e.currentTarget.selectionStart
+		distanceFromEnd = stringValue.length - (cursorPosition ?? 0)
+		const temp = localeStringToNumber(stringValue, formatter)
+		if (temp === undefined) {
+			console.error('Not a number')
+		} else {
+			console.log('Valid number')
+			value = temp
+			stringValue = numberToLocalString(value, formatter)
+		}
+		setTimeout(() => {
+			updateCursorPosition()
+		}, 0)
+	}}
+	{dimension}
+	{disabled}
+	{buttons}
+	{...restProps}
+/>
