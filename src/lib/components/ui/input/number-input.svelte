bb3edc31998ca9d210ca9fe3d046087d0b4181be
Tomáš Hlávka <137988631+hlavkat@users.noreply.github.com>
2024-09-24 08:32:49 +0200

    fix: optional button controls at number input (#350)
    * fix: optional button controls at number input
    
    * fix: control buttons are not displayed by default

--- src/lib/components/ui/input/number-input.svelte
+++ src/lib/components/ui/input/number-input.svelte
@@ -3,12 +3,16 @@
 	import Button from '../button.svelte'
 	import Input, { type Props } from './input.svelte'
 	import type { HTMLInputAttributes } from 'svelte/elements'
+	interface CustomProps extends Props {
+		showControls?: boolean
+	}
 	let {
 		value = $bindable(0),
+		showControls = false,
 		dimension,
 		disabled,
 		...restProps
-	}: Props & Omit<HTMLInputAttributes, 'type'> = $props()
+	}: CustomProps & Omit<HTMLInputAttributes, 'type'> = $props()
 	let size: 16 | 24 | 32 = $derived(dimension === 'large' ? 32 : dimension === 'small' ? 16 : 24)
 </script>
 
@@ -21,4 +25,11 @@
 	</Button>
 {/snippet}
 
-<Input bind:value {dimension} {disabled} {buttons} {...restProps} type="number" />
+<Input
+	bind:value
+	{dimension}
+	{disabled}
+	buttons={showControls ? buttons : undefined}
+	{...restProps}
+	type="number"
+/>
