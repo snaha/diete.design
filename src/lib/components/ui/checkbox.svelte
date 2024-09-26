2c834b6f417e381367351bbb6a422f6cad88d3e8
hlavkat <hlavkat@gmail.com>
2024-09-26 12:39:43 +0200

    fix: checkbox visual bug on iOS

--- src/lib/components/ui/checkbox.svelte
+++ src/lib/components/ui/checkbox.svelte
@@ -84,10 +84,11 @@
 			}
 		}
 	}
+
 	input[type='checkbox'] {
 		position: relative;
 		appearance: none;
-		z-index: 0;
+		z-index: auto;
 		margin: 0;
 	}
 	input[type='checkbox']:focus {
@@ -130,6 +131,10 @@
 			line-height: var(--line-height);
 			letter-spacing: var(--letter-spacing);
 		}
+		input[type='checkbox'] {
+			width: 1.5rem;
+			height: 1.5rem;
+		}
 		input[type='checkbox']:checked::after {
 			width: 0.5rem;
 			height: 1rem;
@@ -145,6 +150,10 @@
 			font-size: var(--font-size-large);
 			line-height: var(--line-height-large);
 			letter-spacing: var(--letter-spacing-large);
+		}
+		input[type='checkbox'] {
+			width: 2rem;
+			height: 2rem;
 		}
 		input[type='checkbox']:checked::after {
 			border-right-width: 3px;
@@ -164,6 +173,10 @@
 			line-height: var(--line-height);
 			letter-spacing: var(--letter-spacing);
 		}
+		input[type='checkbox'] {
+			width: 1.5rem;
+			height: 1.5rem;
+		}
 		input[type='checkbox']:checked::after {
 			width: 0.5rem;
 			height: 1rem;
@@ -180,6 +193,10 @@
 			font-size: var(--font-size-small);
 			line-height: var(--line-height-small);
 			letter-spacing: var(--letter-spacing-small);
+		}
+		input[type='checkbox'] {
+			width: 1rem;
+			height: 1rem;
 		}
 		input[type='checkbox']:checked::after {
 			border-right-width: 1.5px;
